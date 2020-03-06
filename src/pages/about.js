2457291fe247d2ce937887dcd93a1076ee7be972
Mrschabs <rschabalala@gmail.com>
2020-03-06 12:21:16 +0200

    Creat Page Layout

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+import Layout from '../components/layout'
+
+const About = () => {
+    return (
+        <Layout>
+            <h1>About</h1>
+            <p>I am a technical solutionist fascinated by beautiful ux and ui.</p>
+            <p>Reach out <Link to="/contact">Contact me.</Link></p>
+        </Layout>
+    )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Contact = () => {
+    return (
+        <Layout>
+            <h1>Get in touch with me</h1>
+            <p>rschabalala@gmail.com</p>
+            <p>Need a developer? <a href="https://facebook.com" target="_blank">My socials</a></p>
+        </Layout>
+    )
+}
+
+export default Contact
